1661fe8d17c1c5c4e33f2293dfc85f1e063d8131
HU kaifeng <74716458+Polaris-control@users.noreply.github.com>
2024-03-08 00:16:42 +0900

    Update Jumping Game 2.cpp

--- Jumping Game 2.cpp
+++ Jumping Game 2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <climits>
 #include <algorithm>
@@ -20,7 +20,6 @@ int minJump(vector<int>& nums) {
 
     return dp[n - 1];
 }
-
 int main() {
     int n;
     cout << "Enter the length of the array: ";
@@ -37,3 +36,29 @@ int main() {
 
     return 0;
 }
+
+/*这道题是典型的贪心算法，通过局部最优解得到全局最优解。以下两种方法都是使用贪心算法实现，只是贪心的策略不同。
+在具体的实现中，我们维护当前能够到达的最大下标位置，记为边界。我们从左到右遍历数组，到达边界时，更新边界并将跳跃次数增加 1。
+
+在遍历数组时，我们不访问最后一个元素，这是因为在访问最后一个元素之前，我们的边界一定大于等于最后一个位置，否则就无法跳到最后一个位置了。
+如果访问最后一个元素，在边界正好为最后一个位置的情况下，我们会增加一次「不必要的跳跃次数」，因此我们不必访问最后一个元素.
+
+solution:
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int maxPos = 0, n = nums.size(), end = 0, step = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i + nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++step;
+                }
+            }
+        }
+        return step;
+    }
+};
+
+*/
