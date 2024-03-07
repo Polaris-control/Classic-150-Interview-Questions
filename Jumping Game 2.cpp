8085ffc515902971360fdd74879240fce0bcf0d4
HU kaifeng <74716458+Polaris-control@users.noreply.github.com>
2024-03-07 17:15:04 +0900

    Add files via upload

---
+++ Jumping Game 2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int minJump(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n, INT_MAX);
+    dp[0] = 0;
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (j + nums[j] >= i) {
+                dp[i] = min(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return dp[n - 1];
+}
+
+int main() {
+    int n;
+    cout << "Enter the length of the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    int minJumps = minJump(nums);
+    cout << "Minimum jumps required to reach the last index: " << minJumps << endl;
+
+    return 0;
+}
