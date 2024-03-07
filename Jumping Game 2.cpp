6eeaa4796641cf431eaedcb0d659abf33fa8ec8c
HU kaifeng <74716458+Polaris-control@users.noreply.github.com>
2024-03-08 00:17:10 +0900

    Update Jumping Game 2.cpp

--- Jumping Game 2.cpp
+++ Jumping Game 2.cpp
@@ -43,7 +43,7 @@ int main() {
 在遍历数组时，我们不访问最后一个元素，这是因为在访问最后一个元素之前，我们的边界一定大于等于最后一个位置，否则就无法跳到最后一个位置了。
 如果访问最后一个元素，在边界正好为最后一个位置的情况下，我们会增加一次「不必要的跳跃次数」，因此我们不必访问最后一个元素.
 
-solution:
+another solution:
 class Solution {
 public:
     int jump(vector<int>& nums) {
