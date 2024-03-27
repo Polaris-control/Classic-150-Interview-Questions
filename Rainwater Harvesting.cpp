29184bd86ab704b5d73e6a5bd5a80df8e9da6d4f
HU kaifeng <74716458+Polaris-control@users.noreply.github.com>
2024-03-27 16:44:46 +0900

    Update Rainwater Harvesting.cpp

--- Rainwater Harvesting.cpp
+++ Rainwater Harvesting.cpp
@@ -6,7 +6,27 @@
 示例 2：
 
 输入：height = [4,2,0,3,2,5]
-输出：9*/
+输出：9
+
+方法一：动态规划
+
+对于下标 i，下雨后水能到达的最大高度等于下标 iii 两边的最大高度的最小值，下标 i 处能接的雨水量等于下标 i 处的水能到达的最大高度减去 height[i]。
+
+朴素的做法是对于数组 height 中的每个元素，分别向左和向右扫描并记录左边和右边的最大高度，然后计算每个下标位置能接的雨水量。假设数组 height 的长度为 n，该做法需要对每个下标位置使用 O(n) 的时间向两边扫描并得到最大高度，因此总时间复杂度是 O(n^2)。
+
+上述做法的时间复杂度较高是因为需要对每个下标位置都向两边扫描。如果已经知道每个位置两边的最大高度，则可以在 O(n) 的时间内得到能接的雨水总量。使用动态规划的方法，可以在 O(n) 的时间内预处理得到每个位置两边的最大高度。
+
+算法步骤
+创建两个长度为 n 的数组 leftMax 和 rightMax。对于 0 ≤ i < n，leftMax[i] 表示下标 i 及其左边的位置中，height 的最大高度，rightMax[i] 表示下标 i 及其右边的位置中，height 的最大高度。
+计算 leftMax 和 rightMax 的值。
+当 1 ≤ i ≤ n−1 时，leftMax[i] = max(leftMax[i−1], height[i])。
+当 0 ≤ i ≤ n−2 时，rightMax[i] = max(rightMax[i+1], height[i])。
+遍历 height 数组，对于每个位置 i，计算 min(leftMax[i], rightMax[i]) − height[i]，即该位置能接的雨水量，并累加得到总的雨水量。
+复杂度分析
+时间复杂度：O(n)，其中 n 是数组 height 的长度。预处理 leftMax 和 rightMax 数组需要 O(n) 的时间，遍历 height 数组同样需要 O(n) 的时间。
+空间复杂度：O(n)，需要额外的空间存储 leftMax 和 rightMax 数组。
+
+*/
 
 #include <iostream>
 #include <vector>
